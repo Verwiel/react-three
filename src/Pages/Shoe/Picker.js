c8f829d29a560b1f409a277e32babd86d483aebe
unknown <verwiel.code@gmail.com>
2021-02-16 14:30:29 -0700

    seperated shoe out into its own components

---
+++ src/Pages/Shoe/Picker.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { HexColorPicker } from "react-colorful"
+import { useProxy } from "valtio"
+
+function Picker({ state }) {
+  const snap = useProxy(state)
+  return (
+    <div style={{ display: snap.current ? "block" : "none" }}>
+      <HexColorPicker className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
+      <h1>{snap.current}</h1>
+    </div>
+  )
+}
+
+export default Picker

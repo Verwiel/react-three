3f9ef5ffa02424dace6a43d84d91e9f8f560b719
unknown <verwiel.code@gmail.com>
2021-02-16 14:36:56 -0700

    picker only style

--- src/Pages/Shoe/Picker.js
+++ src/Pages/Shoe/Picker.js
@@ -7,7 +7,7 @@ function Picker({ state }) {
   return (
     <div style={{ display: snap.current ? "block" : "none" }}>
       <HexColorPicker className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
-      <h1>{snap.current}</h1>
+      <h1 className="picker-title">{snap.current}</h1>
     </div>
   )
 }

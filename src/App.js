4f9b1067be4836d03ba11beafb36c31fbed597c4
Drew Verwiel <verwiel.code@gmail.com>
2021-02-16 12:50:54 -0700

    added shoe

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react"
 import { Switch, Route } from 'react-router-dom'
 import Home from './Pages/Home/Home.js'
-import Shoe from './Pages/Shoe/Shoe.js'
+import ShoeIndex from './Pages/Shoe/ShoeIndex.js'
 import "./index.css"
 
 function App() {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
-      <Route path='/shoe' component={Shoe} />
+      <Route path='/shoe' component={ShoeIndex} />
     </Switch>
   )
 }
